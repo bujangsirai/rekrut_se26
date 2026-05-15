589d9471c580a3be37deec6b42d9f6e95c21b851
Fatih Mahawisesa <222011585@stis.ac.id>
2026-05-15 09:05:40 +0800

    lanjutkan

--- resources/js/pages/PublicRecruitmentPage.vue
+++ resources/js/pages/PublicRecruitmentPage.vue
@@ -129,6 +129,7 @@ const page = usePage();
                                 </div>
                                 <button
                                     type="submit"
+                                    disabled
                                     class="h-11 cursor-pointer rounded-xl bg-green-600 px-5 text-sm font-semibold text-white transition hover:bg-green-700 sm:h-11 sm:shrink-0 sm:rounded-lg sm:text-base"
                                 >
                                     Cek Status
